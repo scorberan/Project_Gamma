ebdeb5363787c8ba50f885a86dba332dad2d8366
scorberan <sarahcorberan@gmail.com>
2023-05-12 18:59:15 +0200

    router intsall

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Esto está roto</div>,
+    children: [],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
