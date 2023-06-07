9c82be5723e459b1772d19fd749105b06034a9e5
scorberan <sarahcorberan@gmail.com>
2023-06-07 22:05:54 +0200

    made some changes to the swiper gallery

--- src/views/portfolio/Portfolio.jsx
+++ src/views/portfolio/Portfolio.jsx
@@ -1,17 +1,23 @@
 import GradientTitle from "../../components/titles/GradientTitle";
 import "./portfolio.scss";
 import ButtonMedium from "../../components/buttons/ButtonMedium";
-
+import SwiperGallery from "../../components/gallery/SwiperGallery";
 function Portfolio() {
   return (
     <>
       <section id="portfolio">
-        <div>
-          <GradientTitle>My case studies</GradientTitle>
-          <h3 className="title">Portfolio</h3>
+        <div className="section-portfolio">
+          <div className="titles-content">
+            <GradientTitle>My case studies</GradientTitle>
+            <h3 className="title">Portfolio</h3>
+          </div>
+          <div className="swiper-content">
+            <SwiperGallery></SwiperGallery>
+          </div>
+          <div className="btn-content">
+            <ButtonMedium className="inside-content">See more!</ButtonMedium>
+          </div>
         </div>
-
-        <ButtonMedium>See more!</ButtonMedium>
       </section>
     </>
   );
