f61f722edde8ba0cfddd0dbbaaf64548ced560a7
scorberan <sarahcorberan@gmail.com>
2023-06-10 22:10:19 +0200

    starting the structure for the indivuals projects and adding the information and styles

--- src/views/project-beta/ProjectBeta.jsx
+++ src/views/project-beta/ProjectBeta.jsx
@@ -1,10 +1,6 @@
 import "./beta.scss";
-//import ImgReseluta from "../../assets/images/iphone-mockup.png";
-//import GradientTitle from "../../components/titles/GradientTitle";
 import Topbar from "../../components/topbar/Topbar";
 import Footer from "../footer/Footer";
-//import ProjectBackgroundTop from "../../components/projects-background/ProjectBackground_Top";
-//import ProjectBackgroundBottom from "../../components/projects-background/ProjectBackground_Bottom";
 
 function ProjectBeta() {
   return (
@@ -22,7 +18,49 @@ function ProjectBeta() {
           <div className="background-beta-purple">
             <div>
               <div className="info-title">
-                <h3>Reseluta?Who are we?</h3>
+                <div className="beta-title-container">
+                  <h1 className="number-title">01</h1>
+                  <h2 className="beta-title">Resoluta</h2>
+                </div>
+                <div className="beta-info-container">
+                  <div className="info-purple">
+                    <h3 className="text-title">Who are we?</h3>
+                    <p className="beta-text">
+                      We are a professional web development company, with
+                      various services. Providing customised solutions to meet
+                      our user's needs.
+                    </p>
+                    <p className="beta-text">
+                      Our team of experienced professionals are dedicated to
+                      keeping up to date with the latest trends and technologies
+                      to deliver exceptional results.
+                    </p>
+                  </div>
+                  <div className="info-purple">
+                    <h3 className="text-title">What we provide</h3>
+                    <p className="beta-text">
+                      We offer a 24hr service, where any type of customer can
+                      contact our customer can contact our experts to help them
+                      with all their help them with all their emergencies
+                      related with UI&UX, Front-End, WordPress and Marketing.
+                    </p>
+                    <p className="beta-text">
+                      With the guarantee that all your problems will be solved
+                      with maximum speed and above all high quality of work.
+                    </p>
+                  </div>
+                </div>
+                <div className="beta-disclaimer-container">
+                  <div className="disclaimer-info">
+                    <p className="disclaimer-text">
+                      {" "}
+                      "DISCLAIMER" This is a personal project in which I came up
+                      with a service idea and built a website to sell it.
+                      Produced the idea, developing the brand, the illustrations
+                      and the development of the website.
+                    </p>
+                  </div>
+                </div>
               </div>
               <div></div>
             </div>
